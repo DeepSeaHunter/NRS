f5cd8740e97ee8073d9bfe4ff98d8d6f7b99bd6d
unknown <turbozeroa999@gmail.com>
2019-10-18 14:42:44 +0400

    M

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 
 const server=express();
-const port=process.env.port || 3001
+const port=process.env.PORT || 3001
 
 server.use(express.static(path.join(__dirname,'/redirecapp/build')))
 
